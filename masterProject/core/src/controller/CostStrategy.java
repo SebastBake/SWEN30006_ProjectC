7623f779358b038dd02e0bba097a5bdcc6033b14
Sebastian Baker <bakersebastian00@gmail.com>
2017-05-28 12:26:48 +1000

    finished pathfinding

--- masterProject/core/src/controller/CostStrategy.java
+++ masterProject/core/src/controller/CostStrategy.java
@@ -1,7 +1,9 @@
 package controller;
+import utilities.Coordinate;
 
 public interface CostStrategy {
 	
 	public float travelCost(Node fromNode, Node toNode);
+	public float travelCost(Coordinate from, Node toNode);
 	
 }
