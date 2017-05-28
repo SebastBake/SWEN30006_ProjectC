f2dff91947fe962b801b700332b3c909d6406ed5
Sebastian Baker <bakersebastian00@gmail.com>
2017-05-28 12:34:56 +1000

    small changes to strategies

--- masterProject/core/src/controller/CompositeCostStrategy.java
+++ masterProject/core/src/controller/CompositeCostStrategy.java
@@ -1,12 +1,15 @@
 package controller;
 
+import utilities.Coordinate;
 
-public class CompositeCostStrategy implements CostStrategy {
+public abstract class CompositeCostStrategy implements CostStrategy {
 
 	@Override
-	public float travelCost(Node fromNode, Node toNode) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	public abstract float travelCost(Node fromNode, Node toNode);
+	
+	@Override
+	public abstract float travelCost(Coordinate from, Node toNode);
+	
+	
 
 }
